f664928c074620139745091efbb5ebaea93c859c
tcap-knowell <knowell-cedrich.riego@transcosmos.com.ph>
2018-12-04 19:28:42 +0800

    Updates

---
+++ precache-manifest.36e3ff591d7ae16ef8972a125f179f2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "790188b95d2ce1906d45",
+    "url": "/dev-portfolio/static/js/runtime~main.790188b9.js"
+  },
+  {
+    "revision": "ece442d6ca92659ffcca",
+    "url": "/dev-portfolio/static/js/main.ece442d6.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/dev-portfolio/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "ece442d6ca92659ffcca",
+    "url": "/dev-portfolio/static/css/main.b30a7f0a.chunk.css"
+  },
+  {
+    "revision": "de6c95e4bdf6ea6d8c9a20eb890948dc",
+    "url": "/dev-portfolio/index.html"
+  }
+];
